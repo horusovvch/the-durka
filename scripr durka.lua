91c2bff36405b33b8cd02d0054d19ccf735331b5
horusovvch <268007391+horusovvch@users.noreply.github.com>
2026-09-19 02:52:31 +0300

    Create scripr durka.lua
    h

---
+++ scripr durka.lua
@@ -0,0 +1,2 @@
+SCRIPT_KEY = "aa83e524-4d94-4b66-a1e9-d08335a70ddd"
+loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a916ab67aecf1d311b1b02b6bbc907ed8dab95cb7a4323023fea59cdd461af2d/download"))()
