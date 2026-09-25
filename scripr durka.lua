ca564b514375b2ed138a9744b1eb3d6a78be39a0
horusovvch <268007391+horusovvch@users.noreply.github.com>
2026-09-25 03:55:19 +0300

    Update scripr durka.lua

--- scripr durka.lua
+++ scripr durka.lua
@@ -1,2 +1,2 @@
-SCRIPT_KEY = "KEYLESS"
+SCRIPT_KEY = "eb9ec072-2f21-4fd4-8dec-38eaba736a25"
 loadstring(game:HttpGet("https://api.jnkie.com/api/v1/luascripts/public/a916ab67aecf1d311b1b02b6bbc907ed8dab95cb7a4323023fea59cdd461af2d/download"))()
